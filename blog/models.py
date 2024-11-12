398476d47d779ca5ac20d7e6bac46eeb8bd542d8
Bethan Cottell <cottellbc@gmail.com>
2024-11-12 12:00:40 +0000

    comments model added

--- blog/models.py
+++ blog/models.py
@@ -13,3 +13,13 @@ class Post(models.Model):
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True) # auto now add is default created time, time of post entry.
     status = models.IntegerField(choices=STATUS, default=0)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="commenter")
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
